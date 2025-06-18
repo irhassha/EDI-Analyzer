3c0863fb4f23b7a03c3a22e782963f74dc94c0ce
irhassha <irhassha@gmail.com>
2025-06-18 11:57:48 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import io # Diperlukan untuk proses download file Excel
 
 # Konfigurasi halaman Streamlit
 st.set_page_config(page_title="EDI Bay/POD Parser", layout="centered")
